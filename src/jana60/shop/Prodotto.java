7b0c65f4f796d2f30256c2f705e2ef33d6a20343
Luniophel <lucarello.stefanoemanuele@gmail.com>
2022-06-15 11:09:04 +0200

    added constructor to 'Prodotto()'

--- src/jana60/shop/Prodotto.java
+++ src/jana60/shop/Prodotto.java
@@ -2,4 +2,23 @@ package jana60.shop;
 
 public class Prodotto {
 
+	// Attributi
+	String marca;
+	String nome;
+	int prezzo;
+	int iva;
+
+	// Costruttori
+	Prodotto(String marca, String nome, int prezzo, int iva) {
+		this.marca = marca;
+		this.nome = nome;
+		this.prezzo = prezzo;
+		this.iva = iva;
+	}
+
+	// Metodi
+	// Calcola il prezzo comprensivo di iva
+	// Formatta il prezzo restituendo una stringa con numero fisso di decimali e
+	// carattere €
+
 }
